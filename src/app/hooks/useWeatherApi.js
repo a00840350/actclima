2c41ca4d74dbffeeaf8da49c480ca5dce0f8192d
a00840350 <a00840350@tec.mx>
2025-03-03 12:07:33 -0600

    Clima

---
+++ src/app/hooks/useWeatherApi.js
@@ -0,0 +1,5 @@
+import React, { useState } from "react";
+
+const API_KEY = "6e3ae0dd734249e0866132159250303";
+const BASE_URL = "https://api.weatherapi.com/v1/current.json";
+
---
+++ src/app/responses/weather.ts
@@ -0,0 +1,5 @@
+interface WeatherResponse{
+    last_updated: string,
+    last_updated_epoch: number,
+    temp_c: number
+}
